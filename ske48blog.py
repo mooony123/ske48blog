5b807a7378c26f6aee9b4e1c3e7ecd661fff4ed8
mooony <77639338+mooony123@users.noreply.github.com>
2022-05-02 21:29:52 +0800

    async

--- ske48blog.py
+++ ske48blog.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os
 import pprint
 import re
 import requests
 import pytz
+import json
+import asyncio
+import aiohttp
 
 tz = pytz.timezone('Japan')
 BASE_URL='https://ske48.co.jp'
@@ -14,12 +18,16 @@ DATE = 'date'
 TEXT = 'text'
 IMAGES = 'images'
 
-import pprint
+JSON_FILE_NAME = 'ske48blog.json'
+latest_blogs = {}
+
 
 def parse_blog(url: str) -> dict:
-    return_dict = {TEXT: '', IMAGES: []}
+    return_dict = {TITLE: '', AUTHOR: '', DATE: '', TEXT: '', IMAGES: []}
 
     page = requests.get(url)
+    if page.status_code != requests.codes.ok:
+        return {}
     soup = BeautifulSoup(page.content, 'html.parser')
 
     blog_section = soup.find('section', class_='section--detail')
@@ -40,7 +48,56 @@ def blog_to_str(blog: dict) -> str:
                  f'{blog[TEXT]}'
     return return_str
 
+async def get_new_blogs() -> list:
+    retval = []
+
+    page = requests.get('https://ske48.co.jp/blog/list/3/0/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    async def get(name, aiohttpsession):
+        if name.get_text() == 'ALL':
+            return (0, None)
+        ident = name['value'].split('/')[-2]
+        async with aiohttpsession.get(url=(BASE_URL+name['value'])) as resp:
+            ipage = await resp.text()
+        return (ident, ipage)
+    async with aiohttp.ClientSession() as session:
+        responses = await asyncio.gather(*[get(name, session) for
+                                         name in soup.find('select').find_all('option')])
+    for ident, ipage in responses:
+        if ident == 0:
+            continue
+        isoup = BeautifulSoup(ipage, 'html.parser')
+        first = isoup.find('a', class_='clearfix')
+        if first == None:
+            continue
+        url = BASE_URL+first['href']
+        if ident not in latest_blogs or latest_blogs[ident] != url:
+            latest_blogs[ident] = url
+            retval.append(latest_blogs[ident])
+    if len(retval):
+        with open(JSON_FILE_NAME, 'w') as f:
+            json.dump(latest_blogs, f)
+
+    return retval
+
+async def init():
+    global latest_blogs
+    if os.path.isfile(JSON_FILE_NAME):
+        with open(JSON_FILE_NAME, 'r') as f:
+            latest_blogs = json.load(f)
+    else:
+        await get_new_blogs()
+
+async def main():
+    await init()
+    new_blogs = await get_new_blogs()
+    if not len(new_blogs):
+        new_blogs.append(latest_blogs[next(iter(latest_blogs))])
+    for url in new_blogs:
+        blog_dict = parse_blog(url)
+        print(blog_to_str(blog_dict))
+
 if __name__ == '__main__':
-    blog_dict = parse_blog('https://ske48.co.jp/blog/detail/77510/')
-    pprint.pprint(blog_dict)
-    pass
+    asyncio.run(main())
+
